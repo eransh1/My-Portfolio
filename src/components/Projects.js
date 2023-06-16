3a407d899f72014e24b2b16f6da2e1b9b1a7ddea
eransh1 <eranshbansal@gmail.com>
2023-06-16 15:42:53 +0530

    integrate reshuffle project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import "../css/Projects.css"
 
 
 const Projects = ({setProjectInfo}) => {
-const userDoc=useSelector((state)=>state.userDoc)
+const userDocProjectData=useSelector((state)=>state.userDoc.projectData)
+const[projectArray,setProjectArray]=useState([])
+
+useEffect(()=>{
+    if(userDocProjectData){
+        let tempArr=[]
+        userDocProjectData.map((data)=>{return tempArr.unshift(data)})
+        // console.log("tempArr",tempArr)
+        setProjectArray(tempArr)
+ 
+    }
+
+},[userDocProjectData])
+
 function handleInfoClick(item){
     setProjectInfo(item)
 }
@@ -14,14 +27,14 @@ function handleInfoClick(item){
    <section id='projects'>
     <h2 className='project-title'>Portfolio</h2>
     <div className="row project-row">
-    {userDoc?.projectData?.map((item)=>{
+    {projectArray?.map((item)=>{
         return (<>
             <div className="col-sm-12 col-md-6 col-lg-4 project-col" key={item.id}>
         <img className='project-img' src={item.img} alt="react-img" />
         <h4 className='project-name'>{item.title}</h4>
         <div className='linkssss'>
         <button onClick={()=>handleInfoClick(item)} className='info-link'>INFO</button>
-        <a className='project-link' href={item.link} target="_blank">VISIT</a>
+        <a className='project-link' href={item.link} target="_blank" rel="noreferrer">VISIT</a>
         </div>
     </div>
         </>)
